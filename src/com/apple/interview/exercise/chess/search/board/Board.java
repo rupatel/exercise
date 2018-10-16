f2d2039d8d8c114555e4180ce2b3a6246e302fe8
rupatel <patel.ru@husky.neu.edu>
2018-10-15 22:01:12 -0400

    A* implementation

--- src/com/apple/interview/exercise/chess/search/board/Board.java
+++ src/com/apple/interview/exercise/chess/search/board/Board.java
@@ -3,7 +3,10 @@ package com.apple.interview.exercise.chess.search.board;
 import com.apple.interview.exercise.chess.search.cell.Cell;
 import com.apple.interview.exercise.chess.search.piece.PieceType;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.function.Function;
 
 public class Board {
@@ -37,13 +40,44 @@ public class Board {
             int d2 = b.getDistFromStart() + heuristic.apply(b.getCell());
             return d1+d2;
         });
-        return 0;
+        Set<Node> close_set = new HashSet<>();
+        pq.offer(new Node(0,start));
+        Node goalState = null;
+        while(!pq.isEmpty()){
+            Node p = pq.poll();
+            close_set.add(p);
+            if(p.getCell().equals(goal)){
+                goalState = p;
+                break;
+            }
+            else{
+                List<Cell> neighbors = pieceType.getNeighbors(p.getCell(),size);
+                for(Cell v : neighbors){
+                    Integer cost = new Integer(p.getDistFromStart() + 1);
+                    Node c = new Node(cost,v);
+                    if(!close_set.contains(c)){
+                        c.setParent(p);
+                        pq.offer(c);
+                    }
+                }
+            }
+        }
+
+        if(goalState != null){
+            System.out.println("Min dist travelled:" +  goalState.getDistFromStart());
+            return goalState.getDistFromStart();
+        }
+        else{
+            System.out.println("goal state cannnot be reached");
+            return -1;
+        }
     }
 }
 
 class Node{
     private int distFromStart;
     private Cell cell;
+    private Node parent;
     public Node(int distFromStart,Cell cell){
         this.cell = cell;
         this.distFromStart = distFromStart;
@@ -63,5 +97,13 @@ class Node{
 
     public void setCell(Cell cell) {
         this.cell = cell;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
     }
 }
